d8fd7792b15384d9c15189bcd330b2db65b100ee
Abdulla-Alhashmi <92121412+Abdulla-Alhashmi@users.noreply.github.com>
2021-12-23 17:39:30 +0400

    Add files via upload

---
+++ D5HighestScore.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+x = [0]
+for score in student_scores:
+    if score >= x[0]:
+        x[0] = score
+print(f" The highest score is: {score}")
+    
+    
